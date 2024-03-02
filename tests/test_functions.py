4732320a65aef72272126edb41f470f903aafe04
Yoshiki Matsuda <y_matsuda@fixstars.com>
2024-03-02 23:05:11 +0900

    Add built-in constants of `true_`, `false_` and `none` types

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -437,3 +437,14 @@ def test40_nb_signature():
                 None,
             ),
         )
+
+def test41_constant_values():
+    assert t.test_41(True) is True
+    assert t.test_41(False) is False
+    assert t.test_41(None) is None
+    assert t.test_41() is None
+    assert (
+        t.test_41.__doc__ == "test_41(arg: typing.Literal[True], /) -> typing.Literal[True]\n"
+        "test_41(arg: typing.Literal[False], /) -> typing.Literal[False]\n"
+        "test_41(arg: Optional[typing.Literal[None]] = None) -> typing.Literal[None]"
+    )
